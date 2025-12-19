39486bb640152a3508edb3cdf21dc485632a3d64
bhargavkundrapu <kundrapubhargav@gmail.com>
2025-12-19 20:11:02 +0530

    Remove Sentry test route 1.0

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,9 +1,4 @@
 
-const Sentry = require("@sentry/node");
-
-
-
-
 
 
 const express = require("express");
@@ -13,10 +8,7 @@ const app = express();
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
-Sentry.init({ dsn: process.env.SENTRY_DSN });
-app.get("/debug-sentry", (req, res) => {
-  throw new Error("Sentry backend test");
-});
+
 
 
 const PORT = process.env.PORT || 5000;
