b6cf66de7b0e0a6455eb6af6df0b56aced27fe62
bhargavkundrapu <kundrapubhargav@gmail.com>
2025-12-19 18:46:15 +0530

    Add debug-sentry route for testing

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,3 +1,5 @@
+const Sentry = require("@sentry/node");
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 const express = require("express");
 
 const app = express();
@@ -5,6 +7,10 @@ const app = express();
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("Sentry backend test");
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
