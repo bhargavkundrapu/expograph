c4be287125136c170a073919ed5f0b86a8ee5cb6
bhargavkundrapu <kundrapubhargav@gmail.com>
2025-12-19 19:53:20 +0530

    Add Sentry backend test route 4

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,4 +1,6 @@
 
+const Sentry = require("@sentry/node");
+
 
 
 
@@ -11,7 +13,10 @@ const app = express();
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
-
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("Sentry backend test");
+});
 
 
 const PORT = process.env.PORT || 5000;
